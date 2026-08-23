d1ad1ecee468f0a8374c3d013318c89f43b60928
farhannn.11 <mohammadfarhankhan413@gmail.com>
2026-08-24 02:31:29 +0530

    Implement code changes to enhance functionality and improve performance

--- Frontend/src/features/posts/pages/Feed.jsx
+++ Frontend/src/features/posts/pages/Feed.jsx
@@ -3,6 +3,7 @@ import "../style/feed.scss"
 import Post from "../components/Post";
 import { usePost } from "../hooks/usePost";
 import Nav from "../../shared/components/Nav";
+import Login from "../../auth/pages/Login";
 
 const Feed = () => {
   const {loading,feed,post,getPostFeedHandler,postLikeHandler,postUnLikeHandler}  = usePost()
@@ -13,7 +14,7 @@ const Feed = () => {
      
   }, [])
   if(loading || !feed){
-    return <h1>Loading....</h1>
+    return <Login />
   }  
 
   
